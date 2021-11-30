4d28128d67f45abf4605bca9475a827ac524d35e
JordanEspitalier <jordan.espitalier1@gmail.com>
2021-11-30 16:23:58 +0100

    added transition landing => Home

--- src/views/Landing.vue
+++ src/views/Landing.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <transition name="fade">
-      <section>
+      <section v-bind:class="{effect : activeTransition}">
         <h1>WELCOME</h1>
         <p @click="goToHomePage">ENTER</p>
       </section>
@@ -18,20 +18,30 @@ export default {
   components: {},
   data() {
     return {
-      visible: true,
+      activeTransition: false,
     };
   },
   mounted() {},
-  computed: {},
+  computed: {
+  },
   methods: {
     goToHomePage() {
-      this.$router.push({ name: "Home" });
+
+      this.activeTransition = true
+      // Timeout of 2sec before changing to the Home view
+      setTimeout(()=>{
+        this.$router.push({ name: "Home" });
+      }, 2000)
+      
     },
   },
 };
 </script>
 
 <style scoped>
+
+
+
 div {
   height: 100vh;
   color: white;
@@ -72,5 +82,15 @@ p {
 p:hover {
   color: black;
   background-color: white;
+}
+
+.effect{
+  animation-duration: 2s;
+  animation-name: getOut;
+}
+@keyframes getOut {
+  from{opacity: 1;}
+  to{opacity: 0;}
+  
 }
 </style>
